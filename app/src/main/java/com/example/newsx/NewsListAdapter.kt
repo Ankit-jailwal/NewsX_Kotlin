c7160dd9347f8180a7d9f837e2a4e9ff33b9f0c0
Ankit Jailwal <jailwalankit@gmail.com>
2022-04-24 23:39:25 +0530

    initial view

---
+++ app/src/main/java/com/example/newsx/NewsListAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.newsx
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsListAdapter(val items: ArrayList<String>): RecyclerView.Adapter<NewsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
+        return NewsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.titleview.text = currentItem
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
+
+class NewsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    val titleview: TextView = itemView.findViewById(R.id.title)
+}
